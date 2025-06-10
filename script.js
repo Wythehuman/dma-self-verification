af9a5ac495149e441be998351c8946460e0a8e45
Wythehuman <waiyanminkhant812@gmail.com>
2025-06-11 01:11:22 +0630

    Add files via upload
    https://docs.github.com/articles/using-pull-requests

--- script.js
+++ script.js
@@ -3,17 +3,14 @@ document.getElementById("verifyForm").addEventListener("submit", function(e) {
   e.preventDefault();
   const nrc = document.getElementById("nrc").value.trim();
   const cert = document.getElementById("certificate").value;
-
   const resultPanel = document.getElementById("resultPanel");
   const resultText = document.getElementById("resultText");
 
   if (nrc && cert) {
-    resultText.innerText = `✅ Verification Success:
-      NRC: ${nrc}
-      Certificate: ${cert}`;
+    resultText.innerText = `✅ Verification Successful!\nName: Mg Mg\nCertificate: ${cert}\nNRC: ${nrc}\nValid Until: 01-Jan-2027`;
     resultPanel.classList.remove("hidden");
   } else {
-    resultText.innerText = "❌ Please fill in all fields correctly.";
+    resultText.innerText = "❌ Please fill in all fields.";
     resultPanel.classList.remove("hidden");
   }
 });
